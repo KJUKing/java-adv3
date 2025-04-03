4d603701e7171ef11b611e582afe9838e9c96282
KyungJu <kyungjurhdqn@gmail.com>
2025-04-04 00:28:09 +0900

    Initial commit

---
+++ src/lambda/start/Ex0Main.java
@@ -0,0 +1,18 @@
+package lambda.start;
+
+public class Ex0Main {
+
+    public static void hello(String str) {
+        System.out.println("프로그램 시작");
+        //변하는 부분 시작
+        System.out.println(str);
+        //변하는 부분 종료
+        System.out.println("프로그램 종료");
+    }
+
+    public static void main(String[] args) {
+        hello("hello java");
+        hello("hello Spring");
+
+    }
+}
