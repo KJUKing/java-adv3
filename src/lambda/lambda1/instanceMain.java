487ca5c227d335e4b3b0ff1cdde05d1cdde08211
KyungJu <kyungjurhdqn@gmail.com>
2025-04-08 00:28:41 +0900

    250408 람다 김영한 수업

---
+++ src/lambda/lambda1/instanceMain.java
@@ -0,0 +1,23 @@
+package lambda.lambda1;
+
+import lambda.Procedure;
+
+public class instanceMain {
+    public static void main(String[] args) {
+        Procedure procedure1 = new Procedure() {
+            @Override
+            public void run() {
+                System.out.println("안녕 람다야");
+            }
+        };
+
+        System.out.println("procedure.getClass() = " + procedure1.getClass());
+        System.out.println("procedure.instance = " + procedure1);
+
+        Procedure procedure2 = () -> {
+            System.out.println("안녕 람다야");
+        };
+        System.out.println("procedure2.getClass() = " + procedure2.getClass());
+        System.out.println("procedure2.instance= " + procedure2);
+    }
+}
